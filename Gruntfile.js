2a326d7504b99ea844465b7a56a98ebd4c73e101
David H. Bronke <dbronke@armt.com>
2016-07-13 12:25:01 -0500

    Added gruntify-eslint to the build process, updated eslint config, and improved grunt config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,28 @@
 'use strict';
-module.exports = function (grunt) {
+module.exports = function(grunt)
+{
     grunt.initConfig({
-      "babel": {
-        options: {
-          sourceMap: true
+        project: {
+            source: 'src/index.js',
+            example: 'index.html',
+            output: 'dist/index.js'
         },
-        dist: {
-          files: {
-            "dist/index.js": "src/index.js"
-          }
+        "babel": {
+            options: {
+                sourceMap: true
+            },
+            library: {
+                src: ['<%= project.source %>'],
+                dest: '<%= project.output %>'
+            }
+        },
+        eslint: {
+            all: ['<%= project.source %>', '<%= project.example %>']
         }
-      }
     });
 
     grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('gruntify-eslint');
 
-    grunt.registerTask("default", ["babel"]);
+    grunt.registerTask("default", ['eslint', 'babel']);
 };
